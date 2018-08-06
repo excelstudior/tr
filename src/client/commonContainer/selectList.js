bc607f343d96b6f44538970df28dc5ba66ec3a15
Ray Yang <ryang@wfsaustralia.com>
2018-08-06 17:59:23 +1000

    Rewrite selectList as a container

---
+++ src/client/commonContainer/selectList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { updateSort } from '../todo/action/mode'
+
+const SelectList = ({ dispatch, values }) => {
+    let selectedOption;
+    return (
+        <select ref={node => selectedOption = node}
+            onChange={e => {
+                e.preventDefault();
+                dispatch(updateSort(null, parseInt(selectedOption.value)))
+            }} >
+            {Object.keys(values).map(key => <option key={values[key]} value={values[key]}>{key}</option>)}
+        </select>
+    )
+}
+
+
+export default connect()(SelectList);
