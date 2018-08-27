d95c7402f52db3bba0d5a504e42f5e4ba988697f
Ray Yang <ryang@wfsaustralia.com>
2018-08-27 14:59:29 +1000

    Add posts components

---
+++ src/client/Reddit/components/Posts.js
@@ -0,0 +1,33 @@
+import React,{Component} from 'react';
+import PropTypes from 'prop-types';
+
+class Posts extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    componentDidMount(){
+      
+    }
+
+    componentDidUpdate(){
+   
+    }
+   
+    render() {
+ 
+        return (
+            <ul>
+                {this.props.post.map((post,i)=>{
+                    <li key={i}>{post.title}</li>
+                })}
+            </ul>
+        )
+    }
+}
+
+Posts.PropTypes={
+    posts:PropTypes.array.isRequired
+}
+
+export default Posts
