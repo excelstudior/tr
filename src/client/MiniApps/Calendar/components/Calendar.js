15f922018b4daf0cefbf176c068a13ac31f148b9
Ray Yang <>
2018-09-29 11:31:45 +1000

    Mark up calendar head control

--- src/client/MiniApps/Calendar/components/Calendar.js
+++ src/client/MiniApps/Calendar/components/Calendar.js
@@ -1,14 +1,45 @@
 import React, { Component } from 'react';
-import './Calendar.css'
+import './Calendar.css';
+import _ from 'lodash';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import TextField from '../../../CommonComponents/TextField';
 
+
+const Year = ({ years, currentYear, classname, onChange }) => {
+
+    return (<select name='year' className={classname} onChange={onChange}>
+        {years.map((year) => {
+            return year === currentYear
+                ? <option selected value={year}>{year}</option>
+                : <option value={year}>{year}</option>
+        })}
+    </select>)
+}
+
+const Month =({currentMonth,classname,onChange})=>{
+    let months=['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
+    return (
+        <select name='month' className={classname} onChange={onChange}>
+        {months.map((month,i) => {
+            return month === currentMonth
+                ? <option selected value={i}>{month}</option>
+                : <option value={i}>{month}</option>
+        })}
+    </select>
+
+    )
+}
 class Calendar extends Component {
     constructor(props) {
         super(props);
+        this.years = _.range(1900, 2100);
+        this.currentYear = new Date().getFullYear();
+        this.currentMonth= new Date().getMonth();
+        this.currentDate =new Date().getDate();
+        this.currentDay= new Date().getDay();
         this.state = {
-
+            year: this.currentYear,
         };
         this.onChange = this.onChange.bind(this);
 
@@ -28,8 +59,16 @@ class Calendar extends Component {
 
     }
 
-    onChange(e) {
+    setCurrentYear(){
 
+    }
+    setCurrentMonth(){
+        
+    }
+    onChange(e) {
+        //console.log(e.targe.value);
+        this.setState({ [e.target.name]: parseInt(e.target.value) })
+        console.log(this.state)
     }
 
 
@@ -40,8 +79,8 @@ class Calendar extends Component {
             <div className='calendar'>
                 <div className='calendar-head'>
                     <button className='btn-cal-prev'>Prev</button>
-                    <span>2018</span>
-                    <span>Feb</span>
+                    <Year classname='btn-cal-year' onChange={this.onChange} years={this.years} currentYear={this.currentYear} />
+                    <Month classname='btn-cal-month' onChange={this.onChange} currentMonth={this.currentMonth}/>
                     <button className='btn-cal-next'>Next</button>
                 </div>
                 <div className='calendar-dayNames'>
