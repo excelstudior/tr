1d1552b2081f6e13ed0ffc6f01a3798693ab5fbf
Ray Yang <>
2018-10-03 23:57:06 +1000

    Working on dashboard page

--- src/client/User/Admin/components/Dashboard.js
+++ src/client/User/Admin/components/Dashboard.js
@@ -3,9 +3,31 @@ import './Dashboard.css'
 import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom';
 
+
+const MenuItem=(props)=>{
+    const isSelected=()=>{
+        if (props.name===props.selected){
+        return props.style
+    } else {
+        return {}
+    }
+}
+    return(
+        <button onClick={props.handleTabsChange} 
+        name={props.name}
+       
+        
+        >Overview</button>
+    )
+}
+
 class Dashboard extends Component {
     constructor(props) {
         super(props);
+        this.state={
+            selectedTab:'overview'
+        }
+        this.handleTabsChange=this.handleTabsChange.bind(this)
     }
 
     componentDidMount() {
@@ -16,14 +38,31 @@ class Dashboard extends Component {
 
     }
 
+    handleTabsChange(e){
+        this.setState({
+            selectedTab:e.target.name,
+        })
+    }
     render() {
+        const {selectedTab}=this.state;
         return (
     
-                <div className='Dashboard'>
+                <div className='dashboard'>
                     <h2>Admin Dashboard</h2>
                     Today is {new Date().toLocaleDateString()} and logged on at {new Date().toLocaleTimeString()}
-                </div>
+                    <div className='dashboard-tabs'>
+                        <ul>
+                            <li><button onClick={this.handleTabsChange} name='overview'>Overview</button></li>
+                            <li><button onClick={this.handleTabsChange} name='users'>Users</button></li>
+                            <li><button onClick={this.handleTabsChange} name='projects'>Projects</button></li>
+                            <li><button onClick={this.handleTabsChange} name='statistics'>Statistics</button></li>
+                        </ul>
+                    </div>
+                        {selectedTab}
+                    <div>
 
+                    </div>
+                </div>
         )
     }
 }
