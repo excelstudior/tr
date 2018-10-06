ef1e38f0a3a2edecf53a8fe84414f000bbf9331e
Ray Yang <>
2018-10-07 01:14:19 +1000

    Create User content

--- src/client/User/Admin/components/Users.js
+++ src/client/User/Admin/components/Users.js
@@ -4,6 +4,58 @@ import PropTypes from 'prop-types';
 import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import { ADD, SAVE, DELETE, EDIT, CANCEL, NONE } from '../../../CommonComponents/Constants';
 
+const Tbl_Users = (props) => {
+
+   //extract component to write controls on checkbox
+
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th><input
+                        type='checkbox'
+                        name='all'
+                        defaultChecked={false}
+                        checked={props.checked}
+                        onChange={props.onChange}
+                        disabled={props.mode === NONE || props.mode === ADD}
+                    /></th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Avatar</th>
+                    <th>Status</th>
+                    <th>Type</th>
+                </tr>
+            </thead>
+            <tbody>
+                {props.users.map((user, i) => {
+                    return <tr>
+                        <td><input
+                            type='checkbox'
+                            name={user._id}
+                            defaultChecked={false}
+                            onChange={props.onChange}
+                            disabled={props.mode === NONE || props.mode === ADD}
+                        /></td>
+                        <td>{user.name}</td>
+                        <td>{user.email}</td>
+                        {/*write a onMouseOver method to display a long value */}
+                        <td>
+                            {user.avatar === null
+                                ? ''
+                                : user.avatar.length >= 6
+                                    ? user.avatar.substring(0, 5)
+                                    : user.avatar
+                            }</td>
+                        <td>{user.isActive?'Active':'Inactive'}</td>
+                        <td>{user.type}</td>git 
+                    </tr>
+                })}
+            </tbody>
+
+        </table>
+    )
+}
 
 class Users extends Component {
     constructor(props) {
@@ -12,7 +64,8 @@ class Users extends Component {
             editUsers: [],
             mode: NONE
         }
-        this.onEdit = this.onEdit.bind(this);
+        this.handleSwitchMode = this.handleSwitchMode.bind(this);
+
 
     }
 
@@ -28,14 +81,13 @@ class Users extends Component {
 
     }
 
-    onEdit() {
+    handleSwitchMode(e) {
         this.setState({
-            editUsers: this.props.users.users,
-            mode: EDIT,
+            //editUsers: [],
+            mode: e.target.name,
         })
         console.log(this.state)
     }
-
     render() {
         const { loading, users } = this.props.users;
         const { editUsers, mode } = this.state;
@@ -44,14 +96,28 @@ class Users extends Component {
             content = <p>Loading...</p>
         } else {
             content =
-                <div className='dashboard-users'>
+                <div className='dashboard-users-content'>
                     There are {users.length} users
-                        </div>
+                    {users.length !== 0
+                        ? <Tbl_Users
+                            mode={mode}
+                            users={users}
+                        />
+                        : ''}
+                </div>
         }
         return (
-            <div>
-                <button onClick={this.onEdit}>Edit</button>
+            //EDIT||mode===ADD||mode===DELETE
+            <div className='dashboard-users'>
+                <div className='dashboard-users-button'>
+                    {mode === NONE ? <button onClick={this.handleSwitchMode} name={ADD}>{ADD}</button> : ''}
+                    {mode === NONE ? <button onClick={this.handleSwitchMode} name={EDIT}>{EDIT}</button> : ''}
+                    {mode === NONE ? <button onClick={this.handleSwitchMode} name={DELETE}>{DELETE}</button> : ''}
+                    {mode === NONE ? '' : <button onClick={this.handleSwitchMode} name={NONE}>{CANCEL}</button>}
+                </div>
+
                 {content}
+                {/* {users.length!==0?<Tbl_Users mode={mode} users={users} />:''} */}
             </div>
         )
     }
